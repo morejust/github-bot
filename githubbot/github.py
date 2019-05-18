342be39b4c0c64c4c8489eb148c9c9bbbe563d1a
Dan Okhlopkov <danokhlopkov@gmail.com>
2019-05-18 16:49:51 +0300

    init codes

---
+++ githubbot/github.py
@@ -0,0 +1,73 @@
+import os
+import time
+import random
+import requests
+from tqdm import tqdm
+
+GITHUB_API_URL = 'https://api.github.com'
+
+class Github:
+    def __init__(self, username: str, password: str) -> None:
+        self.session = requests.Session()
+        self.session.auth = (username, password)
+        self.username = username
+
+    def _handle_http_errors(self, response: requests.Response) -> None:
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as http_error:
+            raise RuntimeError(
+                f"HTTP Error! Status code: {response.status_code}", response.status_code
+            ) from http_error
+                
+    def _get_followers_or_following(self, username, followers_or_following):
+        if followers_or_following not in ["followers", "following"]:
+            raise RuntimeError(
+                f"Param can be only 'followers' or 'following' (not {followers_or_following})"
+            )
+            
+        api_route = 'user' if username == self.username else f'users/{username}'
+        current_url = f'{GITHUB_API_URL}/{api_route}/{followers_or_following}?page=1'
+
+        result = set()
+        while True:
+            current_response = self.session.get(current_url, timeout=5)
+            self._handle_http_errors(current_response)
+
+            result |= set([follower["login"] for follower in current_response.json()])
+
+            try:
+                current_url = current_response.links["next"]["url"]
+            except KeyError:
+                break
+                
+        return result
+        
+    def get_followers(self, username):
+        return self._get_followers_or_following(username, followers_or_following="followers")
+    
+    def get_following(self, username):
+        return self._get_followers_or_following(username, followers_or_following="following")
+        
+    def follow(self, username):
+        response = self.session.put(
+            f"{GITHUB_API_URL}/user/following/{username}", timeout=5
+        )
+        self._handle_http_errors(response)
+        return response
+    
+    def unfollow(self, username):
+        response = self.session.delete(
+            f"{GITHUB_API_URL}/user/following/{username}", timeout=5
+        )
+        self._handle_http_errors(response)
+        return response
+
+    def __del__(self):
+        self.session.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.session.close()
---
+++ githubbot/githubbot.py
@@ -0,0 +1,66 @@
+import os
+import time
+import random
+from tqdm import tqdm
+
+from .github import Github
+
+class GithubBot:
+    def __init__(self, username, password):
+        self.api = Github(username, password)
+
+        self.username = username
+        self.reached_users = self._read_reached_users()
+
+    def _read_reached_users(self):
+        filename ="%s.txt" % self.username
+
+        if not os.path.isfile(filename):
+            return set()
+
+        with open(filename, 'r') as fhandle:  
+            return set([line.rstrip() for line in fhandle.readlines()])
+
+    def _dump_reached_users(self):
+        filename = "%s.txt" % self.username
+
+        with open(filename, 'w') as fhandle:
+            fhandle.writelines("%s\n" % user for user in self.reached_users)  
+        
+    def follow(self, username):
+        self.api.follow(username)
+        self.reached_users.add(username)
+        self.sleep()
+        
+    def unfollow(self, username):
+        self.api.unfollow(username)
+        self.reached_users.add(username)
+        self.sleep()
+        
+    def sleep(self, max_val=30, min_val=3):
+        time.sleep(min_val + max_val * random.random())
+        
+    def update_reached_users(self):
+        self.reached_users |= self.api.get_followers(self.api.username) | self.api.get_following(self.api.username)
+        print("Users reached: %d" % len(self.reached_users))
+        self._dump_reached_users()
+        
+    def follow_user_followers(self, username):
+        followers = self.api.get_followers(username)
+        for user_to_follow in tqdm(
+            followers, 
+            total=len(followers),
+            desc="%s's followers" % username
+        ):
+            self.follow(user_to_follow)
+            
+    def unfollow_everyone(self):
+        i_will_follow_them = self.api.get_following(self.api.username)
+        for user_to_unfollow in tqdm(i_will_follow_them):
+            self.unfollow(user_to_unfollow)
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exception_type, exception_value, traceback):
+        self._dump_reached_users()

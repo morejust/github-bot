9ec9d56a46a0c8ffd0b552d6772f505f958a4929
Dan Okhlopkov <danokhlopkov@gmail.com>
2019-05-18 17:53:33 +0300

    work with several your followers

--- follow_your_follower_followers.py
+++ follow_your_follower_followers.py
@@ -3,13 +3,17 @@ import random
 
 from githubbot import GithubBot
 
+FOLLOWERS_TO_FOLLOW_THEIR_FOLLOWERS = 2
+
 with GithubBot(
     username=os.environ.get('GITHUB_USERNAME'),
     password=os.environ.get('GITHUB_TOKEN')
 ) as gh:
-    # gh.update_reached_users()
     my_followers = gh.api.get_followers(gh.username)
-    username = random.sample(my_followers, 1)[0]
+    users = random.sample(
+        my_followers, FOLLOWERS_TO_FOLLOW_THEIR_FOLLOWERS
+    )
 
-    print("Going to follow %s's followers:" % username)
-    gh.follow_user_followers(username)
+    for username in users:
+        print("Going to follow %s's followers:" % username)
+        gh.follow_user_followers(username)
